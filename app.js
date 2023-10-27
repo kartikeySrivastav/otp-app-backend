01cdc013ff9f1769383026b9835b34bfeb1afdf0
kartikey srivastava <kartikey1425@gmail.com>
2023-10-27 08:41:26 +0530

    issue fixed cors

--- app.js
+++ app.js
@@ -16,5 +16,11 @@ app.use(
 		useTempFiles: true,
 	})
 );
-app.use(cors());
+app.use(
+	cors({
+		origin: "http://localhost:3000",
+		methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+		credentials: true,
+	})
+);
 app.use("/api/user", userRouter);
